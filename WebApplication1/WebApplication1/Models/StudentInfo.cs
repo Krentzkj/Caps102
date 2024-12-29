3aa71b79292657c9180ae50db457c75bbccab5d0
JayvePogi319 <jayvejamesjauod@gmail.com>
2024-12-29 12:09:49 -0800

    Update

--- WebApplication1/WebApplication1/Models/StudentInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class StudentInfo
-    {
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string DeptDescription { get; set; }
-    }
-}
